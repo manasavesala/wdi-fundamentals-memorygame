721157c28e942a3191e1378907f5d89fee31c304
Murali Medisetty <murali.medisetty@gmail.com>
2018-09-03 16:42:45 -0700

    created game logic

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -1,8 +1,25 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards=["queen", "queen", "king", "king"];
+
+var cardsInPlay=[];
+var cardOne;
+
+cardOne= cards[2];
+
+cardsInPlay.push(cardOne);
+
 
 console.log("User flipped " + cardOne);
+
+var cardTwo;
+cardTwo= cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length===2){
+  if(cardsInPlay[0]==cardsInPlay[1]){
+    alert("you found a match");
+  }else if(cardsInPlay[0]!==cardsInPlay[1]){
+    alert("Sorry,try again.");
+  }
+}
