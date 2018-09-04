3458db09297a9baa2a6b93f0c0a6da9e60aa1528
Murali Medisetty <murali.medisetty@gmail.com>
2018-09-04 15:32:16 -0700

    created game logic

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -1,5 +1,28 @@
 
-var cards=["queen", "queen", "king", "king"];
+var cards=[
+{
+  rank : " queen ",
+  suit : " hearts ",
+  cardImage: "images/queen-of-hearts.png "
+},
+{
+  rank : " queen ",
+  suit : " diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank : " king ",
+  suit : " hearts ",
+  cardImage: " images/king-of-hearts.png"
+},
+{
+  rank: "king" ,
+  suit: "diamonds",
+  cardImage: "images/king-of-dimonds.png"
+}
+];
+
+
 
 var cardsInPlay=[];
 var checkForMatch = function(){
@@ -12,13 +35,16 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
   if(cardsInPlay.length===2){
     checkForMatch();
   }
 
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+   console.log(cards[cardId].cardImage);
+ console.log(cards[cardId].suit);
 }
 
  flipCard(0);
  flipCard(2);
+
